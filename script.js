896066f1d0832b2a568c75ee8672079a9c93005f
Asan <example@gmail.com>
2024-02-21 18:33:20 +0600

    есептер

--- script.js
+++ script.js
@@ -67,12 +67,179 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+
+
+// console.log( NaN || 2 || undefined );//2
+
+
+// console.log( NaN && 2 && undefined ); //NaN
+
+
+// console.log( 1 && 2 && 3 ); //3
+
+
+// console.log( !1 && 2 || !3 ); //false
+
+
+// console.log( 25 || null && !3 ); //25
+
+
+// console.log( NaN || null || !3 || undefined || 5);//5
+
+
+// console.log( NaN || null && !3 && undefined || 5);  //5
+
+
+// console.log( 5 === 5 &&  5 > 1 || 5); //true
+
+
+
+// let result='';
+// for(let i=1;i<7;i++){
+//     for(let j =0;j<i;j++){
+//         result=result +'*';
+//     }
+//     result=result +'\n';
+// }
+// console.log(result);
+
+
+// for (let i = 5; i<=10;i++){
+//     console.log(i);
+// }
+
+
+// for(let i = 20;i > 10; i--){
+//     console.log(i)
+//     if(i===13){
+//         break;
+//     }
+// }
+
+
+// for(let i = 2;i < 10;i+=2){     //(i++) = (i=i+1), (i=i+2) = (i+=2)
+//      console.log(i);
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+// let i =2;
+// while(i < 16){
+//     if(i % 2 === 0){
+//         continue;
+//          i++;    //мынаны жазбасан четныйлар пропускается но мына блок токтамайды потомучто і сол күйі қалады
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+
+
+// function fifthTask() {
+//     const arrayOfNumbers = [];
+//     for(let i=5;i<=10;i++){
+//         arrayOfNumbers.push(i);
+//     }         
+//     return arrayOfNumbers;
+
+
+//     // Пишем решение вот тут
+    
+    
+//     // Не трогаем
+// }
+// console.log(fifthTask());
+
+
+
+// function thirdTask() {
+//     // Пишем решение вот тут
+//     for(let i = 2;i < 10;i+=2)
+//     console.log(i);
+// }
+// thirdTask();
+
+
+
+// function firstTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const arr = [3, 5, 8, 16, 20, 23, 50];
+//     const result = [];
+//     for(let i=0;i<arr.length;i++){
+//         result[i]=arr[i];
+//     }
+//     // Пишем решение вот тут
+    
+    
+//     // Не трогаем
+//     return result;
+// }
+// console.log(firstTask());
+
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for(let i=0;i<data.length;i++){
+//     if(typeof data[i] ==='number'){
+//         data[i]=data[i]*2;
+//     }
+//     else if(typeof data[i] ==='string'){
+//         data[i]=data[i]+ ' done';
+//     }
+// }
+// console.log(data);
+
+
+// function thirdTask() {
+//     // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+//     const data = [5, 10, 'Shopping', 20, 'Homework'];
+//     const result = [];
+//     for(let i=1;i<data.length;i++){
+//         result[i-1]=data[data.length-i];
+//     }
+//     // Пишем решение вот тут
+    
+    
+//     // Не трогаем
+//     return result;
+// }
+// console.log(thirdTask());
+
+
+// const lines = 5;
+// let result = '';
+// for(let i=0;i<=lines;i++){
+//     for(let j=0;j<lines-i;j++){
+//         result=result +' ';
+//     }
+//     for(let j=0;j<2*i+1;j++){
+//         result=result +'*';
+//     }
+//     result+='\n';
+// }
+// console.log(result);
+
+
+// let result='';
+// for(let i=1;i<7;i++){
+//     for(let j =0;j<i;j++){
+//         result=result +'*';
+//     }
+//     result=result +'\n';
+// }
+// console.log(result);
+
 'use strict';
-const numberOfFilms = +prompt("Қанша кино көрдіңіз");
-const lastFilm = prompt("Соңғы көрген кинолардын бірі");
-const reiting = prompt("Баға бериниз", "Мында жаз");
-const lastFilm2 = prompt("Соңғы көрген кинолардын бірі");
-const reiting2 = prompt("Баға бериниз", "Мында жаз");
+const numberOfFilms = prompt('Қанша кино көрдіңіз');
+const lastFilm = prompt('Соңғы көрген кинолардын бірі');
+const reiting = prompt('Баға бериниз', 'Мында жаз');
+const lastFilm2 = prompt('Соңғы көрген кинолардын бірі');
+const reiting2 = prompt('Баға бериниз', 'Мында жаз');
 const personalMovieDB = {
     count:numberOfFilms,
     movies:{},
@@ -83,8 +250,6 @@ const personalMovieDB = {
 personalMovieDB.movies[lastFilm] = reiting;  //точка аркылы жазсан жауап жазган кезде пробелмен берсен жазганын окылмайды, орнына сол переменный ози турады
 personalMovieDB.movies[lastFilm2] = reiting2;
 console.log(personalMovieDB);
-
-
 
 
 
